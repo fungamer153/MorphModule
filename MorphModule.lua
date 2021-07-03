911612086189043d73de2baab4811822d433897f
Allen S. Crow <16709300+colbert2677@users.noreply.github.com>
2021-07-02 22:48:36 -0400

    Technical and repo improvements
    * Add .editorconfig for VS Code users to set file standards
    
    * Add .gitignore for VS Code users to ignore files that should not be included in commits
    
    * Move Instructions from unused local variable in MorphModule scope to README.md
    
    * Detail README.md with use instructions, API and licensing
    
    * Add typechecking to MorphModule; some instances of typechecking are slightly unorthodox
    
    * Divorced constructor from object-level methods to prevent use of module at a class-level and generate potential issues (e.g. nonexistent Folder)
    
    * Simplify constructor
    
    * Covered most cases of potential incorrect use with typechecking and if statements with tracebacks instead of raw warns: not all cases are caught and will error organically
    
    * Used CollectionService to avoid specification of folder from remove functions; more direct and future proofed against name conflictions
    
    * Changed ClearCharacterAppearance to be a local function due to strict internal use - it does not need to be returned with the module because it does not use self nor does it need to be exported for other code
    
    * Underscore SeekFolder to clarify main intent is for internal use with self and not for other code
    
    * Non-xFullMorph methods are now sugar/aliases for calling xFullMorph with the includeClothes arguments as false - avoids rewriting code
    
    * Add Destroy method for Maids and self-cleaning capabilities; removes strong reference to folder from constructor and detaches metatable

---
+++ MorphModule.lua
@@ -0,0 +1,143 @@
+local CollectionService: CollectionService = game:GetService("CollectionService")
+
+local MORPH_OBJECT_TAG_NAME: string = "MorphModuleObject"
+
+local Constructors = {}
+
+local Morph = {}
+Morph.__index = Morph
+
+local function clearCharacterAppearance(character: Model, doAccessories: boolean?, doClothes: boolean?)
+	if not character then
+		warn(debug.traceback("Cannot clear character appearance: no model specified"))
+		return
+	elseif not doAccessories and not doClothes then
+		warn(debug.traceback("No appearance items to clear"))
+		return
+	end
+
+	local humanoid: Instance? = character:FindFirstChildOfClass("Humanoid")
+
+	if doAccessories and humanoid then
+		(humanoid :: Humanoid):RemoveAccessories()
+	end
+
+	if doClothes then
+		for _: number, object: Instance in ipairs((character :: Model):GetChildren()) do
+			if object:IsA("Clothing") then
+				object:Destroy()
+			elseif doAccessories and object:IsA("Accessory") then
+				-- Take advantage of the loop to remove accessories that couldn't be removed
+				-- if a Humanoid didn't exist to call RemoveAccessories on
+				object:Destroy()
+			end
+		end
+	end
+end
+
+-- Required properties like Folder will be nil at a class-level
+-- Expose constructors instead to work with morphs
+function Constructors.new(folder: Folder)
+	if not folder then
+		warn(debug.traceback("No folder specified; no object created from constructor"))
+		return
+	end
+
+	return setmetatable({
+		Folder = folder
+	}, Morph)
+end
+
+function Morph:_SeekFolder(folderName: string): Folder?
+	if not folderName then
+		-- Traceback provided by function calling _SeekFolder
+		return nil
+	end
+
+	for _, object in ipairs(self.Folder:GetDescendants()) do
+		if object:IsA("Folder") and object.Name == folderName then
+			return object
+		end
+	end
+	return nil
+end
+
+function Morph:ApplyFullMorph(player: Player, folderName: string, includeClothes: boolean?)
+	local character: Model
+	local humanoid: Humanoid
+	local selectedFolder: Folder = self:_SeekFolder(folderName)
+
+	if not selectedFolder then
+		warn(debug.traceback(string.format("No morph named %s exists", folderName or "null")))
+		return
+	elseif not player then
+		warn(debug.traceback("No player specified"))
+		return
+	elseif not character then
+		character = player.Character :: Model
+		if not character then
+			warn(debug.traceback(string.format("%s does not have a character", player.Name)))
+			return
+		end
+	else
+		humanoid = character:FindFirstChildOfClass("Humanoid") :: Humanoid
+		if not humanoid then
+			warn(debug.traceback(string.format("%s does not have a Humanoid", player.Name)))
+			return
+		end
+	end
+
+	clearCharacterAppearance((character :: Model), true, includeClothes)
+
+	for _: number, item: Instance in ipairs(selectedFolder:GetChildren()) do
+		if item:IsA("Accessory") then
+			local newAccessory = item:Clone()
+			CollectionService:AddTag(newAccessory, MORPH_OBJECT_TAG_NAME)
+			humanoid:AddAccessory(newAccessory)
+		elseif includeClothes and item:IsA("Clothing") then
+			local newClothing = item:Clone()
+			CollectionService:AddTag(newClothing, MORPH_OBJECT_TAG_NAME)
+			newClothing.Parent = character
+		end
+	end
+end
+
+function Morph:ApplyMorph(player: Player, folderName: string)
+	self:ApplyFullMorph(player, folderName, false)
+end
+
+function Morph:RemoveFullMorph(player: Player, doClothes: boolean?)
+	local character: Model
+
+	if not player then
+		warn(debug.traceback("No player specified"))
+		return
+	elseif not character then
+		character = player.Character :: Model
+		if not character then
+			warn(debug.traceback(string.format("%s does not have a character", player.Name)))
+			return
+		end
+	end
+
+	for _: number, item: Instance in ipairs((character :: Model):GetChildren()) do
+		if item:IsA("Accessory") and CollectionService:HasTag(item, MORPH_OBJECT_TAG_NAME) then
+			item:Destroy()
+		elseif doClothes and item:IsA("Clothing") and CollectionService:HasTag(item, MORPH_OBJECT_TAG_NAME) then
+			item:Destroy()
+		end
+	end
+end
+
+function Morph:RemoveMorph(player: Player)
+	self:RemoveFullMorph(player, false)
+end
+
+function Morph:Destroy()
+	self.Folder = nil
+	setmetatable(self, nil)
+end
+
+print("MorphModule v1 fully loaded!")
+
+return Constructors

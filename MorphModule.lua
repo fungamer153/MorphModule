e28829d9e8cb2571a7ec164e123428ccc3b70e5f
fungamer153 <54368302+fungamer153@users.noreply.github.com>
2022-05-01 14:19:16 +0100

    New functions
    New face functionality and fix to leg problem

--- MorphModule.lua
+++ MorphModule.lua
@@ -1,164 +1,228 @@
-local CollectionService: CollectionService = game:GetService("CollectionService")
-
-local MORPH_OBJECT_TAG_NAME: string = "MorphModuleObject"
-
-local Constructors = {}
-
 local Morph = {}
 Morph.__index = Morph
 
-local function clearCharacterAppearance(character: Model, doAccessories: boolean?, doClothes: boolean?, doFaces: boolean?)
-	if not character then
-		warn(debug.traceback("Cannot clear character appearance: no model specified"))
-		return
-	elseif not doAccessories and not doClothes and not doFaces then
-		warn(debug.traceback("No appearance items to clear"))
-		return
-	end
-
-	local humanoid: Instance? = character:FindFirstChildOfClass("Humanoid")
-
-	if doAccessories and humanoid then
-		(humanoid :: Humanoid):RemoveAccessories()
-	end
-
-	if doClothes then
-		for _: number, object: Instance in ipairs((character :: Model):GetChildren()) do
-			if object:IsA("Clothing") then
-				object:Destroy()
-			elseif doAccessories and object:IsA("Accessory") then
-				-- Take advantage of the loop to remove accessories that couldn't be removed
-				-- if a Humanoid didn't exist to call RemoveAccessories on
-				object:Destroy()
-			end
-		end
-	end
+local Instructions = {
+	[[Thank you for using the MorphModule provided be le_fishe, this module runs serverside meaning that you need to use server scripts in orde to require it
+	or use any of It's functions.
 	
-	if doFaces then
-		for _, Face in ipairs(character.Head):GetChildren()) do
-			if Face:Isa("Texture") or Face:Isa("Decal") then
-				Face:Destroy()
+	*There is no fee or copyright on this meaning that you can use it freely without needing to give me any ownership of your project! but it would be nice
+	of you to at least credit me or keep this message here as it shows appreciation towards my work.
+	
+	**This only works for accessories which are in my opinion the easier option of doing morphs so using models or parts only won't work.**
+	
+	Here is the full list of thing you can do:
+	
+	.new(Morph folder name) -- Will set this as the default folder that all morphs are stored in, only needs to be ran once on a server script for the whole game
+	
+	:ApplyFullMorph(Player instance, Folder name, Clothes: true/false, Faces: true/false) -- This applies whole morph to the directed user in the first argument 
+	:ApplyMorph(Player, Folder name) -- This only applies the morph but can be considered a faster way of applying as it doesn't check if you want clothes with it or not
+	:RemoveWholeMorph(Player, Morph name aka the folder name) -- Removes the whole of the morph including the clothes with it without touching anything else that doesn't exist in the original folder
+	:RemoveMorph(Player, Morph name aka the folder name) -- Removes only the morph without the clothes without touching any other accessories that aren't named the same at least
+	
+	Keep in mind that you need to create a folder inside ServerStorage named `Morphs` and from there you can split morphs into team folders or individual folders 
+	than the script will find itself unless there are duplicate instances of those
+	
+	*Make sure that all of the morphs are unanchored or your character will get stuck in place!!*
+	You can as well insert any shirts or pants inside the folder you want to be applied and they can be named anything you want.
+	]]
+}
+
+
+function Morph.new(MorphFolder)
+	local MorphStuff = {}
+	setmetatable(MorphStuff, Morph)
+
+	MorphStuff.MorphFolder = MorphFolder;
+	
+	return MorphStuff
+end
+
+function Morph:SeekFolder(Folder)
+	for i,v in pairs(self.MorphFolder:GetDescendants()) do
+		if v:IsA('Folder') then
+			if v.Name == Folder then
+				return v
 			end
 		end
 	end
 end
 
--- Required properties like Folder will be nil at a class-level
--- Expose constructors instead to work with morphs
-function Constructors.new(folder: Folder)
-	if not folder then
-		warn(debug.traceback("No folder specified; no object created from constructor"))
-		return
-	end
+function Morph:RemoveNotNeededItems(plr, Accessories, Clothes)
+	for i,Items in pairs(plr.Character:GetChildren()) do
+		if Accessories then
+			if Items:IsA("Accessory") then
+				Items:Destroy()
+			end
+		end
 
-	return setmetatable({
-		Folder = folder
-	}, Morph)
+		if Clothes then
+			if Items:IsA("Pants") or Items:IsA("Shirt") then
+				Items:Destroy()
+			end
+		end
+
+	end
 end
 
-function Morph:_SeekFolder(folderName: string): Folder?
-	if not folderName then
-		-- Traceback provided by function calling _SeekFolder
-		return nil
-	end
+function Morph:ApplyMorph(Player, Folder)
+	local SelectedFolder = self:SeekFolder(Folder)
 
-	for _, object in ipairs(self.Folder:GetDescendants()) do
-		if object:IsA("Folder") and object.Name == folderName then
-			return object
+	assert(Player and SelectedFolder, "Please specify the morph folder you want to use! or Specify the player you're targetting this at!")
+	
+	self:RemoveNotNeededItems(Player, true, false)
+
+	for i,Item in pairs(SelectedFolder:GetChildren()) do
+		if Item:IsA("Accessory") then
+			local MorphPiece = Item:Clone()
+			Player.Character.Humanoid:AddAccessory(MorphPiece)
+			
+			if MorphPiece.Name:gsub("%s", "") == "LeftUpperLeg" or MorphPiece.Name:gsub("%s", "") == "RightUpperLeg" or MorphPiece.Name:gsub("%s", "") == "LeftUpperArm" or MorphPiece.Name:gsub("%s", "") == "RightUpperArm"  then
+				local Attachment = MorphPiece:FindFirstChild(MorphPiece.Name:sub(0, string.find(MorphPiece.Name:gsub("%s", ""), "Upper") - 1).. (string.find(MorphPiece.Name, "Leg") and "HipRigAttachment" or "ShoulderAttachment"), true)
+
+				if Attachment then
+					local CharacterAttachment = nil
+
+					if Player.Character:FindFirstChild(MorphPiece.Name:gsub("%s", "")) then
+						local priorityAttachment = Player.Character[MorphPiece.Name:gsub("%s", "")]:FindFirstChild(Attachment.Name)
+
+						if priorityAttachment then
+							CharacterAttachment = priorityAttachment
+						end
+					end
+
+					if CharacterAttachment then
+						local Weld = Instance.new("Weld")
+
+						Weld.Name = "AccessoryWeld"
+						Weld.C0 = CharacterAttachment.CFrame
+						Weld.C1 = Attachment.CFrame
+						Weld.Part0 = CharacterAttachment.Parent
+						Weld.Part1 = Attachment.Parent
+
+						pcall(function() MorphPiece.Handle:WaitForChild("AccessoryWeld", 1):Destroy() end)
+
+						Weld.Parent = MorphPiece.Handle
+					end
+				end
+			end
 		end
 	end
-	return nil
 end
 
-function Morph:ApplyFullMorph(player: Player, folderName: string, includeClothes: boolean?, includeFaces: boolean?)
-	local character: Model
-	local humanoid: Humanoid
-	local selectedFolder: Folder = self:_SeekFolder(folderName)
+function Morph:RemoveMorph(Player, MorphName)
+		
+	assert(MorphName and Player, "Please make sure you pass over Player instance and morph you wanted to remove string")
+	
+	local SelectedFolder = self:SeekFolder(MorphName)
+	local MorphItems = {}
 
-	if not selectedFolder then
-		warn(debug.traceback(string.format("No morph named %s exists", folderName or "null")))
-		return
-	elseif not player then
-		warn(debug.traceback("No player specified"))
-		return
-	elseif not character then
-		character = player.Character :: Model
-		if not character then
-			warn(debug.traceback(string.format("%s does not have a character", player.Name)))
-			return
+	for i,v in pairs(SelectedFolder:GetChildren()) do
+		if v:IsA('Accessory') then
+			table.insert(MorphItems, v.Name)
 		end
+	end 
+
+	for i,v in pairs(Player.Character:GetChildren()) do
+		if v:IsA("Accessory") then
+			if table.find(MorphItems, v.Name) then
+				v:Destroy()
+			end		
+		end
+	end		
+end
+
+function Morph:RemoveWholeMorph(Player, MorphName)
+	
+		assert(Player and MorphName, "Please make sure you pass over Player instance and morph you wanted to remove string")
+	
+		local SelectedFolder = self:SeekFolder(MorphName)
+		local MorphItems = {}
+
+		for i,v in pairs(SelectedFolder:GetChildren()) do
+			if v:IsA('Accessory') or v:IsA("Pants") or v:IsA("Shirt") then
+				table.insert(MorphItems, v.Name)
+			end
+		end 
+
+		for i,v in pairs(Player.Character:GetChildren()) do
+			if v:IsA("Accessory") or v:IsA("Pants") or v:IsA("Shirt") then
+				if table.find(MorphItems, v.Name) then
+					v:Destroy()
+				end	
+			end
+		end		
+end
+
+function Morph:ApplyFullMorph(Player, Folder, Clothes, faces)
+	local SelectedFolder = self:SeekFolder(Folder)
+	
+	assert(Player and SelectedFolder, "Please specify the morph folder you want to use! or Specify the player you're targetting this at!")
+
+	if SelectedFolder == nil then
+		assert("Specified folder doesn't exist. Please create the folder and use a string for the folder's name.")
 	else
-		humanoid = character:FindFirstChildOfClass("Humanoid") :: Humanoid
-		if not humanoid then
-			warn(debug.traceback(string.format("%s does not have a Humanoid", player.Name)))
-			return
-		end
-	end
+		self:RemoveNotNeededItems(Player, true, true)
+		for i,Item in pairs(SelectedFolder:GetChildren()) do
+			if Clothes == nil then
+				assert("Please state if you want clothes to be applied as well. false = no, true = yes")
+			else
+				if Clothes then
+					if Item:IsA("Pants") or Item:IsA("Shirt") then
+						local ClonedUniform = Item:Clone()
+						ClonedUniform.Parent = Player.Character
+					end
+				end
+				
+				if faces then
+					if Player.Character.Head:FindFirstChildOfClass('Decal') then
+						Player.Character.Head:FindFirstChildOfClass('Decal'):Destroy()
+					end
+					
+					if Item:IsA('Decal') then
+						local NewFace = Item:Clone()
+						NewFace.Parent = Player.Character.Head
+					end
+				end
 
-	clearCharacterAppearance((character :: Model), true, includeClothes)
+				if Item:IsA("Accessory") then
+					local MorphPiece = Item:Clone()
+					Player.Character.Humanoid:AddAccessory(MorphPiece)
+					
+					if MorphPiece.Name:gsub("%s", "") == "LeftUpperLeg" or MorphPiece.Name:gsub("%s", "") == "RightUpperLeg" or MorphPiece.Name:gsub("%s", "") == "LeftUpperArm" or MorphPiece.Name:gsub("%s", "") == "RightUpperArm"  then
+						local Attachment = MorphPiece:FindFirstChild(MorphPiece.Name:sub(0, string.find(MorphPiece.Name:gsub("%s", ""), "Upper") - 1).. (string.find(MorphPiece.Name, "Leg") and "HipRigAttachment" or "ShoulderAttachment"), true)
 
-	for _: number, item: Instance in ipairs(selectedFolder:GetChildren()) do
-		if item:IsA("Accessory") then
-			local newAccessory = item:Clone()
-			CollectionService:AddTag(newAccessory, MORPH_OBJECT_TAG_NAME)
-			humanoid:AddAccessory(newAccessory)
-		elseif includeClothes and item:IsA("Clothing") then
-			local newClothing = item:Clone()
-			CollectionService:AddTag(newClothing, MORPH_OBJECT_TAG_NAME)
-			newClothing.Parent = character
-		elseif includeFaces and item:IsA("Decal") or item:IsA("Texture") then
-			local NewFace = item:Clone()
-			CollectionService:AddtAG(NewFace, MORPH_OBJECT_TAG_NAME)
-			NewFace.Parent = character.Head
-		end
-	end
-end
+						if Attachment then
+							local CharacterAttachment = nil
 
-function Morph:ApplyMorph(player: Player, folderName: string)
-	self:ApplyFullMorph(player, folderName, false)
-end
+							if Player.Character:FindFirstChild(MorphPiece.Name:gsub("%s", "")) then
+								local priorityAttachment = Player.Character[MorphPiece.Name:gsub("%s", "")]:FindFirstChild(Attachment.Name)
 
-function Morph:RemoveFullMorph(player: Player, doClothes: boolean?, doFaces: boolean?)
-	local character: Model
+								if priorityAttachment then
+									CharacterAttachment = priorityAttachment
+								end
+							end
 
-	if not player then
-		warn(debug.traceback("No player specified"))
-		return
-	elseif not character then
-		character = player.Character :: Model
-		if not character then
-			warn(debug.traceback(string.format("%s does not have a character", player.Name)))
-			return
-		end
-	end
+							if CharacterAttachment then
+								local Weld = Instance.new("Weld")
 
-	for _: number, item: Instance in ipairs((character :: Model):GetChildren()) do
-		if item:IsA("Accessory") and CollectionService:HasTag(item, MORPH_OBJECT_TAG_NAME) then
-			item:Destroy()
-		elseif doClothes and item:IsA("Clothing") and CollectionService:HasTag(item, MORPH_OBJECT_TAG_NAME) then
-			item:Destroy()
-		end
-	end
+								Weld.Name = "AccessoryWeld"
+								Weld.C0 = CharacterAttachment.CFrame
+								Weld.C1 = Attachment.CFrame
+								Weld.Part0 = CharacterAttachment.Parent
+								Weld.Part1 = Attachment.Parent
 
-	if doFaces then
-		for _, Face in ipairs(character):GetChildren()) do
-			if Face:IsA("Texture") or Face:Isa("Decal") then
-				Face:Destroy()
+								pcall(function() MorphPiece.Handle:WaitForChild("AccessoryWeld", 1):Destroy() end)
+
+								Weld.Parent = MorphPiece.Handle
+							end
+						end
+					end
+				end
 			end
 		end
 	end
-
-end
-
-function Morph:RemoveMorph(player: Player)
-	self:RemoveFullMorph(player, false)
-end
-
-function Morph:Destroy()
-	self.Folder = nil
-	setmetatable(self, nil)
 end
 
 print("MorphModule v1 fully loaded!")
 
-return Constructors
+return Morph
